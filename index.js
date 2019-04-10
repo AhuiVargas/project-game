d8c923dd773a3eb7c03bd5210dac522fc0afc120
AhuiVargas <ahuijr@gmail.com>
2019-04-10 13:42:08 -0500

    added music

--- index.js
+++ index.js
@@ -4,16 +4,33 @@ const stripeimg = "images/stripe.png";
 let frames = 0;
 let interval;
 const obstacles = [];
-//let obstaclesArray = [];
+let intro = new Audio();
+intro.src = "Music/Ahuizotl - Neon (Intro).mp3";
+intro.loop = true;
+intro.currentTime = 0;
+let startMusic = new Audio();
+startMusic.src = "Music/Ahuizotl - Neon (Game On Fix).mp3";
+startMusic.loop = true;
+startMusic.currentTime = 0;
 
 window.onload = () => {
   const canvas = document.querySelector("canvas");
   const ctx = canvas.getContext("2d");
+  intro.play();
 
   //CLASES
 
+  class Board{
+    constructor(){
+        this.x = 0;
+        this.y = 0;
+        this.width = canvas.width;
+        this.height = canvas.height;
+    }
+  }
+
   class Player {
-    constructor(img, x=400, y=250) {
+    constructor(img, x, y) {
       this.x = x;
       this.y = y;
       this.img = new Image();
@@ -23,7 +40,7 @@ window.onload = () => {
       };
     }
     draw() {
-      ctx.drawImage(this.img, this.x, this.y, 40, 40);
+      ctx.drawImage(this.img, this.x, this.y, 30, 30);
     }
 
     moveUp() {
@@ -40,10 +57,10 @@ window.onload = () => {
     }
     isTouching(obstacle) {
       return (
-        this.x < obstacle.x + obstacle.width &&
-        this.x + 50 > obstacle.x &&
-        this.y < obstacle.y + obstacle.height &&
-        this.y + 50 > obstacle.y
+        this.x <= obstacle.x + obstacle.width &&
+        this.x + 30 >= obstacle.x &&
+        this.y <= obstacle.y + obstacle.height &&
+        this.y + 30 >= obstacle.y
       );
     }
   }
@@ -67,15 +84,6 @@ window.onload = () => {
       if (this.type) {
         ctx.drawImage(this.img1, this.x, this.y, this.width, this.height);
       } else {
-        //console.log(
-        //  this.x,
-        //  this.y,
-        //  this.width,
-        //  this.height,
-        //  this.type,
-        //  this.img1.src
-        //);
-
         ctx.drawImage(this.img1, this.x, this.y, this.width, canvas.height);
       }
       // this.x--;
@@ -101,15 +109,6 @@ window.onload = () => {
       if (this.type) {
         ctx.drawImage(this.img1, this.x, this.y, this.width, this.height);
       } else {
-        //console.log(
-        //  this.x,
-        //  this.y,
-        //  this.width,
-        //  this.height,
-        //  this.type,
-        //  this.img1.src
-        //);
-
         ctx.drawImage(this.img1, this.x, this.y, canvas.width, this.height);
       }
       // this.x--;
@@ -121,14 +120,15 @@ window.onload = () => {
 
   //FLUJO PRINCIPAL
   function update() {
+    let activeObstacles = obstacles.slice(-5);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //board.draw()
     player.draw();
-    generateStripe();
-    obstacles.slice(-4).forEach(obstacle => {
+    activeObstacles.forEach(obstacle => {
       obstacle.draw();
     });
-    checkCollition();
+    checkCollition(activeObstacles);
+    generateStripe();
     //drawObstacles()
 
     //if (frames % 60 === 0);
@@ -139,6 +139,8 @@ window.onload = () => {
   function startGame() {
     if (interval) return;
     interval = setInterval(update, 1000 / 60);
+    intro.pause();
+    startMusic.play();
   }
 
   //HELPERS
@@ -146,11 +148,11 @@ window.onload = () => {
   function generateStripe() {
     const randomHeight = Math.floor(Math.random() * 50);
     const randomWidth = Math.floor(Math.random() * 50);
-    if (frames > 0 && frames % 60 === 0) {
-      const obs1 = new Stripe(randomHeight, 400, false);
+    if (frames > 87 && frames % 30 === 0) {
+      const obs1 = new Stripe(randomHeight, 700, true);
       obstacles.push(obs1);
     }
-    if (frames > 0 && frames % 90 === 0) {
+    if (frames > 87 && frames % 60 === 0) {
       const obs2 = new StripeY(400, randomWidth, true);
       obstacles.push(obs2);
     }
@@ -159,11 +161,12 @@ window.onload = () => {
   function gameOver() {
     clearInterval(interval);
     ctx.fillText("Game over", canvas.width / 2 - 20, canvas.height / 2);
+    startMusic.pause();
     // buttonStart.style.display = "block";
   }
 
-  function checkCollition() {
-    obstacles.forEach(obstacle => {
+  function checkCollition(activeObstacles) {
+    activeObstacles.forEach(obstacle => {
       if (player.isTouching(obstacle)) gameOver();
     });
   }
@@ -177,18 +180,16 @@ window.onload = () => {
         startGame();
         break;
       case 38:
-      if(player.y > 0) player.moveUp();
+        if (player.y > 0) player.moveUp();
         break;
       case 40:
-      if(player.y <= canvas.height) player.moveDown();
+        if (player.y <= canvas.height) player.moveDown();
         break;
       case 37:
-      if(player.x > 0)
-        player.moveLeft();
+        if (player.x > 0) player.moveLeft();
         break;
       case 39:
-      if(player.x <= canvas.width)
-        player.moveRight();
+        if (player.x <= canvas.width) player.moveRight();
         break;
     }
   });
