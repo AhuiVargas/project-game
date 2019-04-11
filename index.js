192f3ca9ef2aaef960c2bbb828c29faf2b1e682e
AhuiVargas <ahuijr@gmail.com>
2019-04-10 21:24:06 -0500

    still stucked

--- index.js
+++ index.js
@@ -16,16 +16,17 @@ startMusic.currentTime = 0;
 window.onload = () => {
   const canvas = document.querySelector("canvas");
   const ctx = canvas.getContext("2d");
+  // const ctxLetras = fillStyle ="white"
   intro.play();
 
   //CLASES
 
-  class Board{
-    constructor(){
-        this.x = 0;
-        this.y = 0;
-        this.width = canvas.width;
-        this.height = canvas.height;
+  class Board {
+    constructor() {
+      this.x = 0;
+      this.y = 0;
+      this.width = canvas.width;
+      this.height = canvas.height;
     }
   }
 
@@ -56,12 +57,13 @@ window.onload = () => {
       this.x -= 25;
     }
     isTouching(obstacle) {
-      return (
-        this.x <= obstacle.x + obstacle.width &&
-        this.x + 30 >= obstacle.x &&
-        this.y <= obstacle.y + obstacle.height &&
-        this.y + 30 >= obstacle.y
-      );
+      if (obstacle.type === true)
+        return (
+          this.x <= obstacle.x + obstacle.width &&
+          this.x + 30 >= obstacle.x + 50 &&
+          this.y <= obstacle.y + obstacle.height &&
+          this.y + 30 >= obstacle.y
+        );
     }
   }
 
@@ -73,20 +75,21 @@ window.onload = () => {
       this.width = Math.floor(Math.random() * 50);
       this.height = height;
       this.type = type;
-      this.img1 = new Image();
-      this.img1.src = stripeimg;
-      //this.generateStripe()
-      //this..onload = () => {
-      //  this.draw();
-      //};
+      //this.img1 = new Image();
+      //this.img1.src = stripeimg;
     }
+
     draw() {
-      if (this.type) {
-        ctx.drawImage(this.img1, this.x, this.y, this.width, this.height);
-      } else {
-        ctx.drawImage(this.img1, this.x, this.y, this.width, canvas.height);
-      }
-      // this.x--;
+      //    if (this.type) {
+      //      ctx.drawImage(this.img1, this.x, this.y, this.width, this.height);
+      //    } else {
+      //ctx.drawImage(this.img1, this.x, this.y, this.width, canvas.height);
+
+      ctx.fillRect(this.x, this.y, this.height, canvas.width);
+      //  }
+    }
+    color(color) {
+      ctx.fillStyle = color;
     }
   }
 
@@ -98,22 +101,20 @@ window.onload = () => {
       this.width = width;
       this.height = Math.floor(Math.random() * 50);
       this.type = type;
-      this.img1 = new Image();
-      this.img1.src = stripeimg;
-      //this.generateStripe()
-      //this..onload = () => {
-      //  this.draw();
-      //};
+      //this.img1 = new Image();
+      //this.img1.src = stripeimg;
     }
     draw() {
-      if (this.type) {
-        ctx.drawImage(this.img1, this.x, this.y, this.width, this.height);
-      } else {
-        ctx.drawImage(this.img1, this.x, this.y, canvas.width, this.height);
-      }
-      // this.x--;
+      //if (this.type) {
+      //  ctx.drawImage(this.img1, this.x, this.y, this.width, this.height);
+      //} else {
+      ctx.fillRect(this.x, this.y, canvas.width, this.height);
+    }
+    color(color) {
+      ctx.fillStyle = color;
     }
   }
+  // }
 
   //DEFINICIONES
   const player = new Player(hexagon, 0, 105);
@@ -125,14 +126,13 @@ window.onload = () => {
     //board.draw()
     player.draw();
     activeObstacles.forEach(obstacle => {
+      //obstacle.draw("rgba(255, 44, 153, 0.4)");
       obstacle.draw();
+      obstacle.color("white");
     });
+    isSolid();
     checkCollition(activeObstacles);
     generateStripe();
-    //drawObstacles()
-
-    //if (frames % 60 === 0);
-    //obstaclesArray.unshift(new Obstacle(300));
     frames++;
   }
 
@@ -149,20 +149,37 @@ window.onload = () => {
     const randomHeight = Math.floor(Math.random() * 50);
     const randomWidth = Math.floor(Math.random() * 50);
     if (frames > 87 && frames % 30 === 0) {
-      const obs1 = new Stripe(randomHeight, 700, true);
+      const obs1 = new Stripe(0, randomWidth, canvas.height, false);
       obstacles.push(obs1);
     }
     if (frames > 87 && frames % 60 === 0) {
-      const obs2 = new StripeY(400, randomWidth, true);
+      const obs2 = new StripeY(0, randomHeight, canvas.width, false);
       obstacles.push(obs2);
     }
   }
+  function isSolid() {
+    setTimeout(() => {
+      obstacles.forEach((obstacles, obs1, obs2) => {
+        if (obs2.type === false) obs2.type = true;
+        obs2.color("rgba(255, 44, 153)");
+        if (obs1.type === false) obs1.type = true;
+        obs1.color("rgba(255, 44, 153)");
+      });
+    }, 30);
+  }
+
+  //function isSolid() {
+  //  if (obsta.type === false)
+  //    setTimeout(function() {
+  //      stripe.type = true;
+  //      ctx.fillStyle = "rgba(255, 44, 153, 0.4)";
+  //    }, 30);
+  //}
 
   function gameOver() {
     clearInterval(interval);
     ctx.fillText("Game over", canvas.width / 2 - 20, canvas.height / 2);
     startMusic.pause();
-    // buttonStart.style.display = "block";
   }
 
   function checkCollition(activeObstacles) {
